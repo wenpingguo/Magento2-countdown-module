96469384dc75c070126598c947467799f870c7cb
guo wenping <guovin@gmail.com>
2019-04-05 16:40:12 +1100

    initial commit

---
+++ MindArc/Countdown/Block/Widget/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace MindArc\Countdown\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Index extends Template implements BlockInterface {
+
+    protected $_template = "widget/index.phtml";
+
+}
---
+++ MindArc/Countdown/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MindArc_Countdown',
+    __DIR__
+);
